ca28470488f52ecb3a27ee8590e5edcf2ce6ef94
Petr Brož <ptrbroz@gmail.com>
2025-12-04 15:14:41 +0100

    remove macro logmsg, go back to function call (timing improvement not worth the uglyness, see previous commit). Also revert basementdevice to pretest state

--- device/BasementDevice/logging.c
+++ device/BasementDevice/logging.c
@@ -6,25 +6,7 @@ LogLevel usbLoggingLevel = info;
 
 const char logLevelChar[] = "diwE~"; //chars used to prefix log messages of corresponding levels
 
-void UNUSED_logmsg(LogLevel level, const char *format, ...){
-    if(level == off){
-        return; //off level meant to be reserved for turning off all logging to selected method, so calling this function with it makes no sense
-    }
-    if(level >= usbLoggingLevel){
-        char prefix = logLevelChar[level];
-        printf("%c]", prefix);
-        char datetimeString[] = "dd.mm.hhmm";     //TODO: get actual datetime once it is known
-        printf("%s:", datetimeString);
-        va_list args;
-        va_start(args, format);
-        vprintf(format, args);
-        va_end(args);
-        printf("\n\r");
-    }
-}
-
-
-void test_logmsg(LogLevel level, const char *format, ...){
+void logmsg(LogLevel level, const char *format, ...){
     if((level) == off){                                     
         return;                                              
     }                                                       
--- device/BasementDevice/logging.h
+++ device/BasementDevice/logging.h
@@ -22,24 +22,7 @@ extern LogLevel usbLoggingLevel;
 // A logging method will only receive the log if @level is greater than or equal to value of the corresponding variable.
 // If called with @level set to off, no log will be produced to any method.
 // \n\r is added to end of logged line automatically and thus shouldn't be included in format
-void UNUSED_logmsg(LogLevel level, const char *format, ...);
-
-void test_logmsg(LogLevel level, const char *format, ...);
-
-// Same as UNUSED_logmsg, but rewritten as macro to avoid overhead. Turns out "function 'logmsg' can never be inlined because it uses variable argument lists"...
-#define logmsg(level, format, ...)                              \
-    do{                                                         \
-        if((level) == off){                                     \
-            break;                                              \
-        }                                                       \
-        if((level) >= usbLoggingLevel){                         \
-            char prefix = logLevelChar[(level)];                \
-            char datetimeString[] = "dd.mm.hhmm"; /*TODO time*/ \
-            printf("%c]%s:", prefix, datetimeString);           \
-            printf((format), ##__VA_ARGS__);                    \
-            printf("\n\r");                                     \
-        }                                                       \
-    } while(0)
+void logmsg(LogLevel level, const char *format, ...);
 
 
 #endif // LOGGING_H
